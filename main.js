59044b7b6a3170f57b6bb3a79c95ee933c9efc9d
ravi <samcollins9899@gmail.com>
2022-11-23 14:40:29 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const API_KEY = "d70119e45ddd9f3893ca56fc14fcb2cb";
+
+const getCurrentWeatherData= async ()=>{
+    const city ="pune";
+
+    console.log(API_KEY, city)
+    const resposnse  = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+    console.log(resposnse)
+    return resposnse.json();
+
+}
+
+
+const loadCurrentForecast = ({main: {temp, temp_max, temp_min, weather:}})
+
+document.addEventListener('DOMContentLoaded', async ()=>{
+     const currentWeather =  getCurrentWeatherData();
+})
